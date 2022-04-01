ca16c8e06b10983088a3d0f83b0a6c42029b9748
linofex <alessandro.noferi@gmail.com>
2022-04-01 16:14:55 +0200

    delete old scenario

--- simulations/NR/mec/highway/videoTraces/videotrace_data_generator/all_videoTraceGenerator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd videoTraces
-for ue in {0..20}
-    do
-        for rep in {0..15}
-            do
-               python3 ../traceDataGenerator.py -f 30 -t 300  -d 0 -o videoTrace_ue[$ue]_rep_$rep
-        done
-done
