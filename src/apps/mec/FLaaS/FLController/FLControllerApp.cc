55e03744874d71c3a2eef1386149e3dbf1b988f4
linofex <alessandro.noferi@gmail.com>
2022-10-05 18:30:11 +0200

    add FLControlleApp

---
+++ src/apps/mec/FLaaS/FLController/FLControllerApp.cc
@@ -0,0 +1,209 @@
+//
+//                  Simu5G
+//
+// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+//
+// This file is part of a software released under the license included in file
+// "license.pdf". Please read LICENSE and README files before using it.
+// The above files and the present reference are part of the software itself,
+// and cannot be removed from it.
+//
+
+#include "apps/mec/FLaaS/FLController/FLControllerApp.h"
+
+#include "nodes/mec/MECOrchestrator/MECOMessages/MECOrchestratorMessages_m.h"
+#include "nodes/mec/MECPlatformManager/MecPlatformManager.h"
+
+Define_Module(FLControllerApp);
+
+
+FLControllerApp::FLControllerApp()
+{
+    baseUriQueries_ = "/example/flController/";
+    baseUriSubscriptions_ = baseUriQueries_;
+    subscriptionId_ = 0;
+    subscriptions_.clear();
+    instantiationMsg_ = nullptr;
+    computationEngineModule_ = nullptr;
+}
+
+
+void FLControllerApp::initialize(int stage)
+{
+    EV << "FLControllerApp::initialize stage " << stage << endl;
+    if (stage == inet::INITSTAGE_LOCAL)
+    {
+        EV << "MecServiceBase::initialize" << endl;
+        serviceName_ = par("serviceName").stringValue();
+
+        requestServiceTime_ = par("requestServiceTime");
+        requestService_ = new cMessage("serveRequest");
+        requestQueueSize_ = par("requestQueueSize");
+
+        subscriptionServiceTime_ = par("subscriptionServiceTime");
+        subscriptionService_ = new cMessage("serveSubscription");
+        subscriptionQueueSize_ = par("subscriptionQueueSize");
+        currentRequestMessageServed_ = nullptr;
+        currentSubscriptionServed_ = nullptr;
+
+        requestQueueSizeSignal_ = registerSignal("requestQueueSize");
+        responseTimeSignal_ = registerSignal("responseTime");
+        binder_ = getBinder();
+
+        instantiationTime_ = par("instantiationTime");
+        instantiationMsg_ = new cMessage("instantiationMsg");
+        scheduleAfter(instantiationTime_, instantiationMsg_);
+    }
+
+    else if (stage == inet::INITSTAGE_APPLICATION_LAYER) {
+        baseSubscriptionLocation_ = host_+ baseUriSubscriptions_ + "/";
+    }
+
+    inet::ApplicationBase::initialize(stage);
+}
+
+void FLControllerApp::handleMessageWhenUp(omnetpp::cMessage *msg)
+{
+    if(msg->isSelfMessage() && msg->isName("instantiationMsg"))
+    {
+        MecAppInstanceInfo* appInfo = instantiateFLComputationEngine();
+        if(appInfo->status)
+        {
+            computationEngineModule_ = appInfo->module;
+            computationEngineModule_->par("controllerModuleName") = getFullPath() ;
+        }
+
+    }
+    else
+    {
+       MecServiceBase::handleMessageWhenUp(msg);
+    }
+}
+
+void FLControllerApp::handleStartOperation(inet::LifecycleOperation *operation)
+{
+    EV << "FLControllerApp::handleStartOperation" << endl;
+    const char *localAddress = par("localAddress");
+    int localPort = par("localUePort");
+    EV << "Local Address: " << localAddress << " port: " << localPort << endl;
+    inet::L3Address localAdd(inet::L3AddressResolver().resolve(localAddress));
+    EV << "Local Address resolved: "<< localAdd << endl;
+
+    // e.g. 1.2.3.4:5050
+    std::stringstream hostStream;
+    hostStream << localAddress<< ":" << localPort;
+    host_ = hostStream.str();
+
+    serverSocket.setOutputGate(gate("socketOut"));
+    serverSocket.setCallback(this);
+    //serverSocket.bind(localAddress[0] ? L3Address(localAddress) : L3Address(), localPort);
+    serverSocket.bind(inet::L3Address(), localPort); // bind socket for any address
+
+    serverSocket.listen();
+}
+
+void FLControllerApp::handleGETRequest(const HttpRequestMessage *currentRequestMessageServed, inet::TcpSocket* socket)
+{
+    EV << "UALCMPApp::handleGETRequest" << endl;
+    std::string uri = currentRequestMessageServed->getUri();
+
+    // check it is a GET for a query or a subscription
+    if(uri.compare(baseUriQueries_+"/model") == 0 ) //queries
+    {
+        std::string params = currentRequestMessageServed->getParameters();
+        if(!params.empty())
+        {
+            std::vector<std::string> queryParameters = lte::utils::splitString(params, "&");
+
+            // parameters can be trainMode or category
+            std::vector<std::string>::iterator it  = queryParameters.begin();
+            std::vector<std::string>::iterator end = queryParameters.end();
+            std::vector<std::string> param;
+            std::vector<std::string> splittedParam;
+
+            // for now it only possible to have ONE parameters
+            for(; it != end; ++it){
+                if(it->rfind("lastTimestamp", 0) == 0) // accessPointId=par1,par2
+                {
+                    EV <<"FLServiceProvider::handleGETReques - parameters: lastTimestamp " << endl;
+                    param = lte::utils::splitString(*it, "=");
+                    if(param.size()!= 2) //must be param=values
+                    {
+                       Http::send400Response(socket);
+                       return;
+                    }
+
+                    // get last model inserted
+                    const auto model = modelHistory_.back();
+                    if(model.timestamp == atoi(param[1]))
+                    {
+                        Http::send200Response(socket, "");
+                    }
+                    else
+                    {
+                        Http::send200Response(socket, "{\"name\": \"send Model with BYTE!\"}");
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            // return last model
+            const auto model = modelHistory_.back();
+
+            Http::send200Response(socket, "{\"name\": \"send Model with BYTE!\"}");
+        }
+    }
+
+    else //bad uri
+    {
+        Http::send404Response(socket);
+
+    }
+
+}
+
+
+MecAppInstanceInfo* FLControllerApp::instantiateFLComputationEngine()
+{
+    // get MEC platform manager and require mec app instantiation
+    cModule* mecPlatformManagerModule = this->getModuleByPath("^.mecPlatformManager");
+    MecPlatformManager* mecpm = check_and_cast<MecPlatformManager*>(mecPlatformManagerModule);
+
+    MecAppInstanceInfo* appInfo = nullptr;
+    CreateAppMessage * createAppMsg = new CreateAppMessage();
+
+//    const char* controllerName = par("controllerName"); // It must be in the form of MECPlatooningControllerRajamani;
+    std::string contName = par("FLComputationEngineApp");
+
+    //get the name of the app
+    std::string appName = contName.substr(contName.rfind(".")+1, contName.length());
+
+    createAppMsg->setUeAppID(par("localUePort")); // TODO choose id
+    createAppMsg->setMEModuleName(appName.c_str());
+    createAppMsg->setMEModuleType(contName.c_str()); //path
+
+    createAppMsg->setRequiredCpu(5000.);
+    createAppMsg->setRequiredRam(10.);
+    createAppMsg->setRequiredDisk(10.);
+    createAppMsg->setRequiredService("NULL"); // insert OMNeT like services, NULL if not
+
+    appInfo = mecpm->instantiateMEApp(createAppMsg);
+
+    if(!appInfo->status)
+    {
+        EV << "FLControllerApp::instantiateFLComputationEngine - something went wrong during MEC app instantiation"<< endl;
+    }
+
+    else
+    {
+        EV << "FLControllerApp::instantiateFLComputationEngine - succesfull MEC app instantiation"<< endl;
+    }
+
+    return appInfo;
+}
+
+
+
+
---
+++ src/apps/mec/FLaaS/FLController/FLControllerApp.h
@@ -0,0 +1,81 @@
+//
+//                  Simu5G
+//
+// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+//
+// This file is part of a software released under the license included in file
+// "license.pdf". Please read LICENSE and README files before using it.
+// The above files and the present reference are part of the software itself,
+// and cannot be removed from it.
+//
+
+
+#ifndef APPS_MEC_FLAAS_FLCONTROLLER_FLCONTROLLERAPP_H_
+#define APPS_MEC_FLAAS_FLCONTROLLER_FLCONTROLLERAPP_H_
+
+
+
+#include "omnetpp.h"
+
+#include "inet/networklayer/common/L3Address.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+
+#include "apps/mec/FLaaS/FLaaSUtils.h"
+
+#include "nodes/mec/MECPlatform/ServiceRegistry/ServiceRegistry.h"
+
+//#include "apps/mec/MecApps/MecAppBase.h"
+#include "nodes/mec/MECPlatform/MECServices/MECServiceBase/MecServiceBase.h"
+
+
+using namespace std;
+using namespace omnetpp;
+
+class MecAppInstanceInfo;
+
+
+class FLControllerApp : public MecServiceBase
+{
+
+    int size_;
+    std::string subId;
+
+    double instantiationTime_;
+    cMessage* instantiationMsg_;
+
+    cModule* computationEngineModule_;
+
+    std::vector<MLModel> modelHistory_;
+    std::vector<LocalLearner> learners_;
+
+    //TODO think about subscriptions
+
+    protected:
+        virtual void initialize(int stage) override;
+        virtual void finish() override;
+
+        virtual void handleMessageWhenUp(omnetpp::cMessage *msg) override;
+        void handleStartOperation(inet::LifecycleOperation *operation) override;
+
+        // GET the list of available MEC app descriptors
+        virtual void handleGETRequest(const HttpRequestMessage *currentRequestMessageServed, inet::TcpSocket* socket) override;
+        // POST the instantiation of a MEC app
+        virtual void handlePOSTRequest(const HttpRequestMessage *currentRequestMessageServed, inet::TcpSocket* socket)   override;
+        // PUT not implemented, yet
+        virtual void handlePUTRequest(const HttpRequestMessage *currentRequestMessageServed, inet::TcpSocket* socket)    override;
+        // DELETE a MEC app previously instantiated
+        virtual void handleDELETERequest(const HttpRequestMessage *currentRequestMessageServed, inet::TcpSocket* socket) override;
+
+        MecAppInstanceInfo* instantiateFLComputationEngine();
+
+    public:
+       FLControllerApp();
+       virtual ~FLControllerApp();
+
+};
+
+
+
+
+
+#endif /* APPS_MEC_FLAAS_FLCONTROLLER_FLCONTROLLERAPP_H_ */
