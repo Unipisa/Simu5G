b1a6cdb44b69f5d6a1534d3e833684f35b796eee
Zoltan Bojthe <zbojthe@omnetpp.org>
2024-06-18 12:27:21 +0200

    typo

--- src/nodes/backgroundCell/BackgroundCellTrafficManager.cc
+++ src/nodes/backgroundCell/BackgroundCellTrafficManager.cc
@@ -120,7 +120,7 @@ Cqi BackgroundCellTrafficManager::computeCqi(int bgUeIndex, Direction dir, inet:
 
 void BackgroundCellTrafficManager::racHandled(MacNodeId bgUeId)
 {
-    Enter_Method("BackgroundTrafficManager::racHandled");
+    Enter_Method("BackgroundCellTrafficManager::racHandled");
 
     int index = bgUeId - BGUE_MIN_ID;
 
