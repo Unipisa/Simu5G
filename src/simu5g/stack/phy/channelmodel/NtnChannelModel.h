9b1bb2e4e80318e3314fc4d17f39d5fc231addf6
Giovanni Nardini <giovanni.nardini@unipi.it>
2026-04-28 14:18:00 +0200

    Replace flat fading with frequency-selective fading model

--- src/simu5g/stack/phy/channelmodel/NtnChannelModel.h
+++ src/simu5g/stack/phy/channelmodel/NtnChannelModel.h
@@ -12,97 +12,88 @@
 #ifndef NTNCHANNELMODEL_H_
 #define NTNCHANNELMODEL_H_
 
+#include <array>
+#include <vector>
+
 #include "inet/common/geometry/common/GeographicCoordinateSystem.h"
 #include "simu5g/stack/phy/channelmodel/LteRealisticChannelModel.h"
 
 namespace simu5g {
 
+struct FrequencySelectiveScenarioParameters
+{
+    std::array<double, 9> muLgDs;
+    std::array<double, 9> sigmaLgDs;
+    std::array<double, 9> muK;
+    std::array<double, 9> sigmaK;
+    std::array<double, 9> rTau;
+    std::array<int, 9> numClusters;
+    bool hasLosComponent;
+};
+
 class NtnChannelModel : public LteRealisticChannelModel
 {
-  public:
-    enum class LmsStateType
-    {
-        GOOD,
-        BAD
-    };
-
-    struct LmsStateParameters
-    {
-        double muDuration;
-        double sigmaDuration;
-        double minDuration;
-        double muMa;
-        double sigmaMa;
-        double h1;
-        double h2;
-        double g1;
-        double g2;
-        double correlationDistance;
-        double transitionSlope;
-        double transitionOffset;
-    };
-
-    struct LmsScenarioParameters
-    {
-        LmsStateParameters good;
-        LmsStateParameters bad;
-        double pBadMin;
-        double pBadMax;
-    };
-
-    struct LmsFadingState
-    {
-        bool initialized = false;
-        bool inTransition = false;
-        LmsStateType state = LmsStateType::GOOD;
-        double remainingDistance = 0.0;
-        double currentMa = 0.0;
-        double currentSigmaA = 0.0;
-        double currentMp = 0.0;
-        double currentCorrelationDistance = 1.0;
-        double directPathAmplitudeDb = 0.0;
-        double transitionLength = 0.0;
-        double transitionProgress = 0.0;
-        double sourceMa = 0.0;
-        double targetMa = 0.0;
-        double sourceSigmaA = 0.0;
-        double targetSigmaA = 0.0;
-        double sourceMp = 0.0;
-        double targetMp = 0.0;
-        double sourceCorrelationDistance = 1.0;
-        double targetCorrelationDistance = 1.0;
-        inet::Coord lastCoord;
-        bool hasLastCoord = false;
-    };
-
   protected:
-    bool useTwoStateModelFading_ = false;
+
     inet::Coord lastTerrestrialEndpointCoord_;
     inet::Coord lastSatelliteEndpointCoord_;
     inet::GeoCoord lastTerrestrialEndpointWgs84_ = inet::GeoCoord::NIL;
     inet::Coord lastSatelliteEndpointEcefCoord_;
     std::map<MacNodeId, double> buildingPenetrationProbabilityMap_;
-    std::map<MacNodeId, LmsFadingState> lmsFadingStateMap_;
 
+    /*
+     * Frequency-selective fading data structures
+     */
+
+    struct FrequencySelectiveFadingState
+    {
+        bool initialized = false;
+        bool los = false;
+        bool isSband = false;
+        int angleIndex = 0;
+        DeploymentScenario scenario = UNKNOWN_SCENARIO;
+        simtime_t referenceTime = SIMTIME_ZERO;
+        double refreshDistance = 0.0;
+        double losLinearPower = 0.0;
+        double losDopplerHz = 0.0;
+        double losInitialPhaseRad = 0.0;
+        inet::Coord lastRefreshCoord;
+        std::vector<double> delaysSeconds;
+        std::vector<double> linearPowers;
+        std::vector<double> dopplerHz;
+        std::vector<double> initialPhaseRad;
+    };
+
+    std::map<MacNodeId, FrequencySelectiveFadingState> fadingStateMap_;
+
+    // compute Line-of-Sight probability and store it into the specific los map
+    void computeLosProbability(double d, MacNodeId nodeId);
+
+    // compute large scale attenuation (path loss, clutter loss, penetration loss, atmospheric, scintillation)
+    double getAttenuation(MacNodeId nodeId, Direction dir, inet::Coord coord, bool cqiDl) override;
+
+    /*
+     * Large-scale model methods
+     */
     double computePathLoss(double distance, double dbp, bool los) override;
     double computeShadowing(double distance, MacNodeId nodeId, double speed, bool cqiDl) override;
     double computeBuildingPenetrationLoss(MacNodeId nodeId);
     double computeAtmosphericLoss();
     double computeScintillationLoss();
-    double computeFastFading(MacNodeId nodeId, double speed, bool cqiDl);
-    const LmsScenarioParameters& getLmsScenarioParameters(double elevationAngle) const;
-    double sampleLmsDuration(const LmsStateParameters& params);
-    double sampleLmsMeanAmplitude(const LmsStateParameters& params, bool isBadState, double pBadMin, double pBadMax);
-    void initializeLmsFadingState(LmsFadingState& state, const LmsScenarioParameters& params);
-    void startLmsTransition(LmsFadingState& state, const LmsScenarioParameters& params);
-    void advanceLmsDirectPath(LmsFadingState& state, double distanceStep);
-    void computeLosProbability(double d, MacNodeId nodeId);
+
+    /*
+     * Frequency-selective fading methods
+     */
+
+    std::vector<double> computeFrequencySelectiveFading(MacNodeId nodeId, double speed);
+    const FrequencySelectiveScenarioParameters& getFrequencySelectiveScenarioParameters(bool los, bool isSband) const;
+    void initializeFrequencySelectiveFadingState(FrequencySelectiveFadingState& state, const FrequencySelectiveScenarioParameters& params, int angleIndex, double maxDopplerHz);
+    double getFadingRefreshDistance(bool los) const;
+    std::vector<double> getBandCenterFrequenciesHz() const;
 
   public:
     void initialize(int stage) override;
 
-    // compute large scale attenuation (path loss, clutter loss, penetration loss, atmospheric, scintillation)
-    double getAttenuation(MacNodeId nodeId, Direction dir, inet::Coord coord, bool cqiDl) override;
     std::vector<double> getSINR(LteAirFrame *frame, UserControlInfo *lteInfo) override;
     std::vector<double> getRSRP(LteAirFrame *frame, UserControlInfo *lteInfo) override;
 };
