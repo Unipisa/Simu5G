ae87842927d1e8962ef8aee3d7bfe811785a65c6
Giovanni Nardini <giovanni.nardini@unipi.it>
2026-04-29 16:25:31 +0200

    Move common antenna parameters and methods to IAntennaModel base class

--- src/simu5g/stack/phy/antennamodel/IsotropicAntennaModel.h
+++ src/simu5g/stack/phy/antennamodel/IsotropicAntennaModel.h
@@ -12,27 +12,9 @@ class IsotropicAntennaModel : public omnetpp::cSimpleModule, IAntennaModel
 
   protected:
 
-    double txPower_;           // in dBm
-    double txFeederLoss_;      // in dB
-    double rxFeederLoss_;      // in dB
-    double rxLumpedLoss_;      // in dB
-    double noiseFigure_;       // in dB
-
     virtual void initialize() override;
 
   public:
-
-    // return the tx power (in dBm)
-    virtual double getTxPower() const { return txPower_; }
-
-    // return the tx feeder loss (in dB)
-    virtual double getTxFeederLoss() const { return txFeederLoss_; }
-    // return the rx feeder loss (in dB)
-    virtual double getRxFeederLoss() const { return rxFeederLoss_; }
-    // return the rx lumped loss (in dB)
-    virtual double getRxLumpedLoss() const { return rxLumpedLoss_; }
-    // return the noise figure (in dB)
-    virtual double getNoiseFigure() const { return noiseFigure_; }
 
     // returns the transmitting antenna gain (in dBi)
     virtual double computeTxGain(double angle = -1.0, double frequency = -1.0)  const override { return 0.0; }
--- src/simu5g/stack/phy/antennamodel/VSATAntennaModel.h
+++ src/simu5g/stack/phy/antennamodel/VSATAntennaModel.h
@@ -12,31 +12,14 @@ class VSATAntennaModel : public omnetpp::cSimpleModule, IAntennaModel
 
   protected:
 
-    double txPower_;           // in dBm
     double efficiency_;
     double diameter_;
-    double txFeederLoss_;      // in dB
-    double rxFeederLoss_;      // in dB
-    double rxLumpedLoss_;      // in dB
-    double noiseFigure_;       // in dB
 
     virtual void initialize() override;
 
     double computeGain(double angle, double frequency) const;
 
   public:
-
-    // return the tx power (in dBm)
-    virtual double getTxPower() const { return txPower_; }
-
-    // return the tx feeder loss (in dB)
-    virtual double getTxFeederLoss() const { return txFeederLoss_; }
-    // return the rx feeder loss (in dB)
-    virtual double getRxFeederLoss() const { return rxFeederLoss_; }
-    // return the rx lumped loss (in dB)
-    virtual double getRxLumpedLoss() const { return rxLumpedLoss_; }
-    // return the noise figure (in dB)
-    virtual double getNoiseFigure() const { return noiseFigure_; }
 
     // returns the transmitting antenna gain (in dBi)
     virtual double computeTxGain(double angle = -1.0, double frequency = -1.0) const override { return computeGain(angle, frequency); }
